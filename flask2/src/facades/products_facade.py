0573021e107855c307d4eb1972e243c72995fb81
Yasmeen Qawasmy <71906328+yasmeenq@users.noreply.github.com>
2024-07-13 21:40:24 +0300

    Add files via upload

---
+++ flask2/src/facades/products_facade.py
@@ -0,0 +1,27 @@
+from logic.products_logic import ProductLogic 
+from flask import request
+from models.product_model import ProductModel
+
+class ProductsFacade:
+
+    def __init__(self):
+        self.logic = ProductLogic() 
+
+    def get_all_products(self):
+        return self.logic.get_all_products() 
+    
+    def get_one_product(self, id):
+        return self.logic.get_one_products(id)
+    
+
+    def add_product(self):
+        name = request.form.get("name") # <input type=text....name = "name" > 
+        price = request.form.get("price") # <input type=number....name = "price" > 
+        stock = request.form.get("stock") # <input type=text....name = "stock" > 
+        image = request.files["image"]  # <input type="file" name="image" >  
+        product = ProductModel(None, name, price, stock, image)
+        self.logic.add_product(product)
+
+
+    def close(self):
+        self.logic.close() 
