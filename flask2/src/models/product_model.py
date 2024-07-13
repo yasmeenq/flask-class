42fb9ef0028b4d2835d4935a53864f3906e7eefd
Yasmeen Qawasmy <71906328+yasmeenq@users.noreply.github.com>
2024-07-13 21:53:51 +0300

    Delete flask2/src directory

--- flask2/src/models/product_model.py
+++
@@ -1,8 +0,0 @@
-class ProductModel:
-
-    def __init__(self, id, name, price, stock, image):
-        self.id = id 
-        self.name = name 
-        self.price = price 
-        self.stock = stock 
-        self.image = image
--- flask2/src/views/about_view.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template 
-
-about_blueprint = Blueprint("about_view", __name__)
-
-@about_blueprint.route("/about")
-def about():
-    return render_template("about.html")
